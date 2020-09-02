3687ceef776998f7aba79ff44867b839154fdba8
rip-phan <rip@rip-phan.dev>
2020-09-01 20:22:04 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,41 @@
+if 5 > 2:
+	print("Five is greater than two!")
+if 5 <2:
+	print("Five is less than two!")
+
+def myfunc(): # defining global func
+	global name
+name="Rip-Phan Python Test Grounds" # MY FILE BRO
+
+# testing multiple line input variable definitiion
+quote=""" 
+bitches know my name
+but they dont
+jokes on them
+"""
+#Python is not monte
+#division addition multiplication defined as "z"
+z=5239/2000+800*55
+#call to global def name
+print(name)
+
+#test run on interpreter output of python basic maths
+print("5239 divided by 2000 plus 800 multiplied by 55 is equal to", z, ".")
+
+#another name test call with age variables, string requires quotation while numeric values do not
+name,age,age2="Joe", 31, 32
+
+z="python is a btich man"
+k="but whatever coz bitches get stitchers"
+m=z+k
+#
+#
+#
+print(m)
+print(m)
+
+
+#run code 
+print("Hello, World!!!",  name, " is my name!, Don't forget that!")
+print("I'm",  age,"--",  age2, "years old and I prolly fuck you up!!")
+print(quote)
